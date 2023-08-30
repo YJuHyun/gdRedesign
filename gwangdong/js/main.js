9ddb12569e1a78b7017cf198a983b9dbc65a636b
YJuHyun <94033837+YJuHyun@users.noreply.github.com>
2023-08-30 09:48:19 +0900

    Update main.js

--- gwangdong/js/main.js
+++ gwangdong/js/main.js
@@ -1,32 +1,4 @@
 $( document ).ready( function() {
-  if($(window).width() > 1024) {
-    $( window ).scroll( function() {
-      if ( $( document ).scrollTop() >  volunteerOffset.top - 400){
-        $('.volunteer').addClass('sectionUp')
-      }else if ( $( document ).scrollTop() >  noticeSecOffset.top - 400){
-        $('.noticeSec').addClass('sectionUp')
-      }else if ( $( document ).scrollTop() > companyOffset.top - 400){
-        $('.icoContainer').addClass('offset'),
-        $('.compTxt').addClass('offsetT')
-      }else if ( $( document ).scrollTop() >  productSecOffset.top - 400){
-        $('.productSec').addClass('sectionUp')
-      }
-    })
-
-  }else {
-    $( window ).scroll( function() {
-      if ( $( document ).scrollTop() >  volunteerOffset.top - 600){
-        $('.volunteer').addClass('sectionUp')
-      }else if ( $( document ).scrollTop() >  noticeSecOffset.top - 600){
-        $('.noticeSec').addClass('sectionUp')
-      }else if ( $( document ).scrollTop() > companyOffset.top - 600){
-        $('.icoContainer').addClass('offset'),
-        $('.compTxt').addClass('offsetT')
-      }else if ( $( document ).scrollTop() >  productSecOffset.top - 600){
-        $('.productSec').addClass('sectionUp')
-      }
-    })
-  }
     var swiper2 = new Swiper(".proSwiper", {
       slidesPerView: 4,
       spaceBetween: 16,
